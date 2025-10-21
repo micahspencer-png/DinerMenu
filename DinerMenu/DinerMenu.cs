e590eef11b0938d82ab20dd1a5f40d957887ea0e
micahspencer-png <micahspencer@isu.edu>
2025-10-21 08:49:24 -0600

    Added methods to store the data. Had the buttons clear and then call for the data

--- DinerMenu/DinerMenu.cs
+++ DinerMenu/DinerMenu.cs
@@ -13,6 +13,24 @@ namespace DinerMenu
             InitializeComponent();
         }
 
+        string Fish() 
+        {
+            string fish = "";
+            fish = "Fresh Caught Fish~\n This is a test";
+            return fish;
+        }
+        string ChefSpecial() 
+        {
+            string chef = "";
+            chef = "Chef's Special";
+            return chef;
+        }
+        string Soup() 
+        {
+            string soup = "";
+            soup = "Soup of The Day";
+            return soup;
+        }
         private void ExitButton_Click(object sender, EventArgs e)
         {
             Close();
@@ -21,19 +39,19 @@ namespace DinerMenu
         private void FishButton_Click(object sender, EventArgs e)
         {
             DisplayItemLabel.Text = string.Empty;
-            DisplayItemLabel.Text = "Fish";
+            DisplayItemLabel.Text = Fish();
         }
 
         private void ChefSpecialButton_Click(object sender, EventArgs e)
         {
             DisplayItemLabel.Text = string.Empty;
-            DisplayItemLabel.Text = "Chef's Special";
+            DisplayItemLabel.Text = ChefSpecial();
         }
 
         private void SoupButton_Click(object sender, EventArgs e)
         {
             DisplayItemLabel.Text = string.Empty;
-            DisplayItemLabel.Text = "Soup of The Day";
+            DisplayItemLabel.Text = Soup();
         }
     }
 }
